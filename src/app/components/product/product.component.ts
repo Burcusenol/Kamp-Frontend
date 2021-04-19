b16f7c594a84359ebddd3398c53d6c91acfe3f09
Burcu ŞENOL <burcusenol62@gmail.com>
2021-04-20 02:55:23 +0300

    20.04.2021

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -14,7 +16,9 @@ export class ProductComponent implements OnInit {
  filterText="";
 
   constructor(private productService:ProductService,
-    private activatedRoute:ActivatedRoute) { }
+    private activatedRoute:ActivatedRoute,
+    private toastrService:ToastrService,
+    private cartService:CartService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params=>{
@@ -40,4 +44,10 @@ export class ProductComponent implements OnInit {
       this.dataLoaded=true;
     })
   }
+addToCart(product:Product)
+{
+  this.toastrService.success("Sepete eklendi.",product.productName)
+  this.cartService.addToCart(product);
+}
+
 }
